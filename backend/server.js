be98ca18b388ee38b06f58f2c93cc57f70985709
naeem4633 <ahmedn3700@gmail.com>
2024-03-08 00:43:53 +0500

    Estacblished database Connection. Connected with mongo db on local host

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const connectDB = require('./dbConnection');
+
 const app = express();
+
+// Connect to MongoDB
+connectDB();
 
 // Define routes here
 
@@ -7,7 +12,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-app.get('/', ()=>{
-    console.log('get');
-})
