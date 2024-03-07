3c72be86e437529bd99c922ea96948f461076ffd
naeem4633 <ahmedn3700@gmail.com>
2024-03-08 03:47:20 +0500

    minor fix, Added campus_id to model too,

--- backend/models/campusModel.js
+++ backend/models/campusModel.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const campusSchema = new mongoose.Schema({
+  campus_id: {
+    type: String,
+    required: true,
+    unique: true
+  },
   name: {
     type: String,
     required: true
