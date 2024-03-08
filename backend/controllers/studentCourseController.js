9e17a9b509019fee19c1bbf52ccf2ed9ff851825
naeem4633 <ahmedn3700@gmail.com>
2024-03-08 18:24:58 +0500

    Modified studentcourses to get student and course objects, given student and course ids

--- backend/controllers/studentCourseController.js
+++ backend/controllers/studentCourseController.js
@@ -1,6 +1,6 @@
-const StudentCourse = require('../models/studentCourseModel');
-const Student = require('../models/studentModel');
 const Course = require('../models/courseModel');
+const StudentCourse = require('../models/studentCourseModel');  
+const Student = require('../models/studentModel');
 
 // Create a new student-course relationship
 const createStudentCourse = async (req, res) => {
@@ -40,13 +40,17 @@ const getAllStudentCourses = async (req, res) => {
   }
 };
 
-// Function to get all course objects given a student ID
 const getCoursesByStudentId = async (req, res) => {
   const { student_id } = req.params;
 
   try {
-    const studentCourses = await StudentCourse.find({ student_id }).populate('course_id');
-    const courses = studentCourses.map(sc => sc.course_id);
+    // Find all courses for the student
+    const studentCourses = await StudentCourse.find({ student_id });
+    const course_ids = studentCourses.map(sc => sc.course_id);
+
+    // Find the courses using the course_ids array
+    const courses = await Course.find({ course_id: { $in: course_ids } });
+
     res.status(200).json(courses);
   } catch (error) {
     console.error('Error getting courses by student ID:', error);
@@ -54,19 +58,24 @@ const getCoursesByStudentId = async (req, res) => {
   }
 };
 
-// Function to get all student objects given a course ID
 const getStudentsByCourseId = async (req, res) => {
   const { course_id } = req.params;
 
   try {
-    const courseStudents = await StudentCourse.find({ course_id }).populate('student_id');
-    const students = courseStudents.map(sc => sc.student_id);
+    // Find all students for the course
+    const studentCourses = await StudentCourse.find({ course_id });
+    const student_ids = studentCourses.map(sc => sc.student_id);
+
+    // Find the courses using the course_ids array
+    const students = await Student.find({ student_id: { $in: student_ids } });
+
     res.status(200).json(students);
   } catch (error) {
     console.error('Error getting students by course ID:', error);
     res.status(500).json({ error: 'Internal server error' });
   }
 };
+
 
 module.exports = {
   createStudentCourse,
